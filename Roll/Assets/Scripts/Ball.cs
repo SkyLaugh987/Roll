0b75f988b76fb70fe135d1b60ca78864364c0be2
QuentinLamy <quentin.lamy16@gmail.com>
2021-10-02 13:30:53 +0200

    protobill

---
+++ Roll/Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    [SerializeField]
+    Rigidbody rb;
+    [SerializeField,Range(1f,10f)]
+    float speed;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(speed*hAxis,0,speed*vAxis),ForceMode.Force);
+    }
+}
