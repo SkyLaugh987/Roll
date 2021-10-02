4638cde5f2f724c241ba0d979c7bd4201390dfd6
Allan <couetteallan@gmail.com>
2021-10-02 14:22:34 +0200

    GameManager + UIManager set up

---
+++ Roll/Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    #region Singleton;
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    #endregion
+
+    void Awake()
+    {
+        _instance = this;
+    }
+}
