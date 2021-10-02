3faaf0c0756eb2eb0fa04fcbc78059374cc6e951
QuentinLamy <quentin.lamy16@gmail.com>
2021-10-02 16:58:59 +0200

    Wind,Void,corr

---
+++ Roll/Assets/Scripts/Void.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Void : MonoBehaviour
+{
+    [SerializeField]
+    Transform respawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Ball ball = other.gameObject.GetComponent<Ball>();
+        if(ball != null){
+            ball.gameObject.transform.position = respawn.position;
+        }
+        //respawn objets
+    }
+}
---
+++ Roll/Assets/Scripts/Wind.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField,Range(0.1f,10f)]
+    float windForce = 1f;
+
+    [SerializeField]
+    Vector3 windDir;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Ball ball = other.gameObject.GetComponent<Ball>();
+        if (ball != null)
+        {
+            ball.gameObject.GetComponent<Rigidbody>().AddForce(windDir * windForce / 5, ForceMode.Acceleration);
+        }
+    }
+}
