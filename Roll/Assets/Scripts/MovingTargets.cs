cf116690608bb33229cb1f3c5d4793abfc0ee361
SkyLaugh987 <skylaughblue@gmail.com>
2021-10-02 17:02:10 +0200

    moving blocs

---
+++ Roll/Assets/Scripts/MovingTargets.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTargets : MonoBehaviour
+{
+    [SerializeField] GameObject[] targets;
+    [SerializeField] float speed;
+    int currIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(targets[currIndex].transform.position, transform.position) < 1)
+        {
+            currIndex++;
+            if (currIndex >= targets.Length)
+                currIndex = 0;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targets[currIndex].transform.position, speed * Time.deltaTime);
+    }
+}
